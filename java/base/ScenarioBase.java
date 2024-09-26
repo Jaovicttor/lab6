db9f42bb45251ef882bf8a827826100fd73ee7de
João Victor <jvdso.15@gmail.com>
2024-09-26 16:21:55 -0300

    add: estrutura scenario1

---
+++ java/base/ScenarioBase.java
@@ -0,0 +1,6 @@
+public class ScenarioBase {
+
+    public static void main(String[] args) {
+        System.out.println("Scenario Base of Lab06!");
+    }
+}
---
+++ java/scenario1/Node.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Node implements Runnable {
+
+    @Override
+    public void run() {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'run'");  
+    }
+}
